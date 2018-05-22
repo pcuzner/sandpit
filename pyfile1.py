55d3049e4621fd4eee81b5a019663c82c76f8dd3
Paul Cuzner <pcuzner@redhat.com>
2018-05-23 10:29:03 +1200

    updatedpyfile

--- pyfile1.py
+++ pyfile1.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python2
 
+import random
+
+
+def dummy():
+    """Short summary.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
+
+
 if __name__ == "__main__":
-    print("hello mr git")
+    print("hello mr git {}".format(random.random()))
